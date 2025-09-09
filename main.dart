0e1c9d0d582cf8c5ed63358ea0c647c37aed5dd0
Shabih e  Zehra <imnoor110@gmail.com>
2025-09-09 09:40:31 +0500

    dart project

--- main.dart
+++ main.dart
@@ -1,10 +1,33 @@
+import 'dart:io';
+import 'dart:math';
+
 void main() {
+  final String textToType = "The quick brown fox jumps over the lazy dog.";
 
+  print("Type the following text as fast and accurately as possible:");
+  print(textToType);
 
+  final Stopwatch stopwatch = Stopwatch()..start();
 
+  final String userInput = stdin.readLineSync()!;
 
+  stopwatch.stop();
 
+  final int typingSpeed = ((textToType.split(" ").length / stopwatch.elapsedMilliseconds) * 60000).round();
+  final int accuracy = calculateAccuracy(textToType, userInput);
 
+  print("Typing speed: $typingSpeed words per minute");
+  print("Accuracy: $accuracy%");
+}
 
-    
+int calculateAccuracy(String expectedText, String userInput) {
+  int errors = 0;
+
+  for (int i = 0; i < min(expectedText.length, userInput.length); i++) {
+    if (expectedText[i] != userInput[i]) {
+      errors++;
+    }
+  }
+
+  return ((1 - (errors / expectedText.length)) * 100).round();
 }
